0ec9920524b39fc91f22db31613074559cd164b3
Hyago Gabriel <oicueio616@gmail.com>
2022-01-14 23:28:47 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,6 +6,7 @@ import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
   const [pageNumber, setPageNumber] = useState(0);
 
   const [page, setPage] = useState<MoviePage>({
@@ -31,9 +32,14 @@ function Listing() {
       });
   }, [pageNumber]);
 
+  const handlePageChange = (newPageNumber : number) => {
+    setPageNumber(newPageNumber);
+  } 
+
   return (
     <>
-      <Pagination />
+      <Pagination page = {page} onChange={handlePageChange}/>
+      
       <div className="container">
         <div className="row">
           {page.content.map((item) => (
